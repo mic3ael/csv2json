2a1dd37f3df2a4a52085041cb49fefc5b2ccd5d0
Michael Horojanski <mic3ael@gmail.com>
2025-02-28 18:15:44 +0200

    chore: added transofrm tests

--- src/transform.js
+++ src/transform.js
@@ -7,50 +7,58 @@ const { threadFactory } = require('./thread.js');
 const { normalizeHeader, aggregator } = require('./utils.js');
 
 class TransformStream extends Transform {
+  #seperator;
+  #headers;
+  #poolSize;
+  #lb;
+  #processesAggregator;
+  #bufferSize;
+  #rest;
+  #isInit;
   constructor({ seperator, headers }) {
     super();
-    this.seperator = seperator;
-    this.headers = headers;
-    this.poolSize = os.cpus().length - 1; // Number of logical processors, - 1 main thread
-    this.lb = null;
-    this.processesAggregator = null;
-    this.bufferSize = 2000;
-    this.rest = '';
-    this.isInit = true;
+    this.#seperator = seperator;
+    this.#headers = headers;
+    this.#poolSize = os.cpus().length - 1; // Number of logical processors, - 1 main thread
+    this.#lb = null;
+    this.#processesAggregator = null;
+    this.#bufferSize = 2000;
+    this.#rest = '';
+    this.#isInit = true;
     this.#setupPoolAndAggregator();
   }
 
   #setupPoolAndAggregator() {
-    if (this.headers.length) {
-      this.lb = new Pool(
-        threadFactory({ headers: this.headers, seperator: this.seperator }, this.poolSize),
-        { size: this.poolSize, timeout: 200 }
+    if (this.#headers.length) {
+      this.#lb = new Pool(
+        threadFactory({ headers: this.#headers, seperator: this.#seperator }, this.#poolSize),
+        { size: this.#poolSize, timeout: 200 }
       );
-      this.processesAggregator = aggregator(this.lb, this.bufferSize);
+      this.#processesAggregator = aggregator(this.#lb, this.#bufferSize);
     }
   }
 
   #complete(result, done, isLast) {
     const str = JSON.stringify(result, null, 2);
     const sliceIdx = isLast ? [1] : [1, -1];
-    if (this.isInit) sliceIdx[0] = 0;
-    this.isInit = false;
+    if (this.#isInit) sliceIdx[0] = 0;
+    this.#isInit = false;
     done(null, str.slice(...sliceIdx));
   }
 
   async _transform(chunk, _, done) {
     try {
-      const text = this.rest + chunk.toString();
+      const text = this.#rest + chunk.toString();
       const lines = text.split(/\r?\n/);
-      if (!this.lb) {
-        this.headers = normalizeHeader(lines.shift(), this.seperator);
+      if (!this.#lb) {
+        this.#headers = normalizeHeader(lines.shift(), this.#seperator);
         this.#setupPoolAndAggregator();
       }
-      if (text.charAt(text.length - 1) === '\n') this.rest = lines.pop();
+      if (text.charAt(text.length - 1) === '\n') this.#rest = lines.pop();
       let result = [];
       for (let i = 0; i < lines.length; i++) {
         const line = lines[i];
-        const jsonArr = await this.processesAggregator.exec(line);
+        const jsonArr = await this.#processesAggregator.exec(line);
         if (!jsonArr) continue;
         result.push(...jsonArr);
       }
@@ -62,13 +70,14 @@ class TransformStream extends Transform {
 
   async _flush(done) {
     try {
-      const jsonArr = await this.processesAggregator.flush(); // Get remaining results
+      if (!this.#processesAggregator) return done();
+      const jsonArr = await this.#processesAggregator.flush(); // Get remaining results
       if (jsonArr) this.#complete(jsonArr.flat(), done, true); // Finalize and close stream
       else done(); // Signal that flush is complete
     } catch (err) {
       done(err); // Handle any errors during flush
     } finally {
-      if (this.lb) await this.lb.cleanup();
+      if (this.#lb) await this.#lb.cleanup();
     }
   }
 }
--- tests/transform.test.js
+++ tests/transform.test.js
@@ -37,58 +37,58 @@ const assert = require('node:assert');
     inputStream.end();
 
     // Test 2: Handling empty input
-    //   const emptyTransformStream = new TransformStream(options);
-    //   const emptyInputStream = new PassThrough();
-    //   const emptyOutputStream = new PassThrough();
-    //
-    //   const emptyInput = '';
-    //   const expectedEmptyOutput = '';
-    //
-    //   let transformedEmptyData = '';
-    //
-    //   emptyOutputStream.on('data', (chunk) => {
-    //     transformedEmptyData += chunk.toString();
-    //   });
-    //
-    //   emptyOutputStream.on('end', () => {
-    //     try {
-    //       assert.strictEqual(transformedEmptyData, expectedEmptyOutput);
-    //       console.log('Test 2: Handling empty input - Passed');
-    //     } catch (error) {
-    //       console.error('Test 2: Handling empty input - Failed', error);
-    //     }
-    //   });
-    //
-    //   emptyInputStream.pipe(emptyTransformStream).pipe(emptyOutputStream);
-    //   emptyInputStream.write(emptyInput);
-    //   emptyInputStream.end();
-    //
-    //   // Test 3: Flushing remaining data
-    //   const flushTransformStream = new TransformStream(options);
-    //   const flushInputStream = new PassThrough();
-    //   const flushOutputStream = new PassThrough();
-    //
-    //   const flushInput = `header1,header2,header3\nvalue1,value2,value3\n`;
-    //   const expectedFlushOutput = `[ { "header1": "value1", "header2": "value2", "header3": "value3" } ]`;
-    //
-    //   let transformedFlushData = '';
-    //
-    //   flushOutputStream.on('data', (chunk) => {
-    //     transformedFlushData += chunk.toString();
-    //   });
-    //
-    //   flushOutputStream.on('end', () => {
-    //     try {
-    //       assert.strictEqual(transformedFlushData, expectedFlushOutput);
-    //       console.log('Test 3: Flushing remaining data - Passed');
-    //     } catch (error) {
-    //       console.error('Test 3: Flushing remaining data - Failed', error);
-    //     }
-    //   });
-    //
-    //   flushInputStream.pipe(flushTransformStream).pipe(flushOutputStream);
-    //   flushInputStream.write(flushInput);
-    //   flushInputStream.end();
+    const emptyTransformStream = new TransformStream(options);
+    const emptyInputStream = new PassThrough();
+    const emptyOutputStream = new PassThrough();
+
+    const emptyInput = '';
+    const expectedEmptyOutput = '';
+
+    let transformedEmptyData = '';
+
+    emptyOutputStream.on('data', (chunk) => {
+      transformedEmptyData += chunk.toString();
+    });
+
+    emptyOutputStream.on('end', () => {
+      try {
+        assert.strictEqual(transformedEmptyData, expectedEmptyOutput);
+        console.log('Test 2: Handling empty input - Passed');
+      } catch (error) {
+        console.error('Test 2: Handling empty input - Failed', error);
+      }
+    });
+
+    emptyInputStream.pipe(emptyTransformStream).pipe(emptyOutputStream);
+    emptyInputStream.write(emptyInput);
+    emptyInputStream.end();
+
+    // Test 3: Flushing remaining data
+    const flushTransformStream = new TransformStream(options);
+    const flushInputStream = new PassThrough();
+    const flushOutputStream = new PassThrough();
+
+    const flushInput = `header1,header2,header3\nvalue1,value2,value3\n`;
+    const expectedFlushOutput = [{ "header1": "value1", "header2": "value2", "header3": "value3" }];
+
+    let transformedFlushData = '';
+
+    flushOutputStream.on('data', (chunk) => {
+      transformedFlushData += chunk.toString();
+    });
+
+    flushOutputStream.on('end', () => {
+      try {
+        assert.deepEqual(JSON.parse(transformedFlushData), expectedFlushOutput);
+        console.log('Test 3: Flushing remaining data - Passed');
+      } catch (error) {
+        console.error('Test 3: Flushing remaining data - Failed', error);
+      }
+    });
+
+    flushInputStream.pipe(flushTransformStream).pipe(flushOutputStream);
+    flushInputStream.write(flushInput);
+    flushInputStream.end();
   } catch (error) {
     console.error('An error occurred during the tests:', error);
   }
